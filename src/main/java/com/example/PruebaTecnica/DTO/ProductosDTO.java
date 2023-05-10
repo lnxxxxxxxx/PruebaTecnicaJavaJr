ab23fa34bc3647ea43434bbafcccec93336bd985
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-05-10 01:04:00 -0300

    add dto mapper and service component

---
+++ src/main/java/com/example/PruebaTecnica/DTO/ProductosDTO.java
@@ -0,0 +1,18 @@
+package com.example.PruebaTecnica.DTO;
+import lombok.*;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductosDTO {
+
+        private Long id;
+        private String nombre;
+        private String descripcion;
+        private float precio;
+        private int cantidad;
+
+
+}
---
+++ src/main/java/com/example/PruebaTecnica/Mapper/ProductosMapper.java
@@ -0,0 +1,30 @@
+package com.example.PruebaTecnica.Mapper;
+
+import com.example.PruebaTecnica.DTO.ProductosDTO;
+import com.example.PruebaTecnica.Model.Productos;
+
+public class ProductosMapper {
+    public static ProductosDTO toDto(Productos payment) {
+        ProductosDTO productosDTO = new ProductosDTO();
+        productosDTO.setId(payment.getId());
+        productosDTO.setNombre(payment.getNombre());
+        productosDTO.setCantidad(payment.getCantidad());
+        productosDTO.setPrecio(payment.getPrecio());
+        productosDTO.setDescripcion(payment.getDescripcion());
+        return productosDTO;
+    }
+
+    public static Productos toModel(ProductosDTO paymentDto) {
+        Productos productos = new Productos();
+        productos.setId(paymentDto.getId());
+        productos.setNombre(paymentDto.getNombre());
+        productos.setCantidad(paymentDto.getCantidad());
+        productos.setPrecio(paymentDto.getPrecio());
+        productos.setDescripcion(paymentDto.getDescripcion());
+        return productos;
+    }
+
+
+
+
+}
